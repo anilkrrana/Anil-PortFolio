42e31d97a48170bd641f53402ff0076f14285c0a
Anil Kumar Rana <anilkrana474@gmail.com>
2023-03-11 20:25:34 +0530

    added alert function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -156,7 +156,8 @@ function Contact() {
                       className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
                   ></textarea>
 
-                  <button className='text-white bg-gradient-to-b from-purple-500 to-orange-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 font-bold'>
+                  <button className='text-white bg-gradient-to-b from-purple-500 to-orange-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 font-bold' 
+                      onChange={handleInputChange}>
                       Let's talk
                   </button>
               </form>
